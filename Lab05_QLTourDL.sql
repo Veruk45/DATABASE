68047521a16cdb4622aa15d8680e4e3a45b155fe
Nhat Quang <95947239+Veruk45@users.noreply.github.com>
2022-03-03 21:44:25 +0700

    Update Lab05_QLTourDL.sql

--- Lab05_QLTourDL.sql
+++ Lab05_QLTourDL.sql
@@ -43,12 +43,12 @@ insert into Tour values ('T003', '5')
 insert into Tour values ('T004', '7')
 select * from Tour
 
-insert into ThanhPho values ('01', 'Đà Lạt')
-insert into ThanhPho values ('02', 'Nha trang')
-insert into ThanhPho values ('03', 'Phan Thiết')
-insert into ThanhPho values ('04', 'Huế')
-insert into ThanhPho values ('05', 'Đà Nẵng')
-insert into ThanhPho values ('01', 'Đà Lạt')
+insert into ThanhPho values ('01', N'Đà Lạt')
+insert into ThanhPho values ('02', N'Nha trang')
+insert into ThanhPho values ('03', N'Phan Thiết')
+insert into ThanhPho values ('04', N'Huế')
+insert into ThanhPho values ('05', N'Đà Nẵng')
+insert into ThanhPho values ('01', N'Đà Lạt')
 select * from ThanhPho
 
 insert into Tour_TP values ('T001', '01', '2')
@@ -65,14 +65,14 @@ select * from Tour_TP
 
 set dateformat dmy
 go
-insert into Lich_TourDL values ('T001', '14/02/2017', N'Vân', '20', 'Nguyễn Hoàng')
-insert into Lich_TourDL values ('T002', '14/02/2017', N'Nam', '30', 'Lê Ngọc')
-insert into Lich_TourDL values ('T002', '06/03/2017', N'Hùng', '20', 'Lý Dũng')
-insert into Lich_TourDL values ('T003', '18/02/2017', N'Dũng', '20', 'Lý Dũng')
-insert into Lich_TourDL values ('T004', '14/02/2017', N'Hùng', '30', 'Dũng Nam')
-insert into Lich_TourDL values ('T003', '10/03/2017', N'Nam', '45', 'Nguyễn An')
-insert into Lich_TourDL values ('T002', '28/04/2017', N'Vân', '25', 'Ngọc Dung')
-insert into Lich_TourDL values ('T004', '29/04/2017', N'Dũng', '35', 'Lê ngọc')
-insert into Lich_TourDL values ('T001', '30/04/2017', N'Nam', '25', 'Trần Nam')
-insert into Lich_TourDL values ('T003', '15/06/2017', N'Vân', '20', 'Trịnh Bá')
+insert into Lich_TourDL values ('T001', '14/02/2017', N'Vân', '20', N'Nguyễn Hoàng')
+insert into Lich_TourDL values ('T002', '14/02/2017', N'Nam', '30', N'Lê Ngọc')
+insert into Lich_TourDL values ('T002', '06/03/2017', N'Hùng', '20', N'Lý Dũng')
+insert into Lich_TourDL values ('T003', '18/02/2017', N'Dũng', '20', N'Lý Dũng')
+insert into Lich_TourDL values ('T004', '14/02/2017', N'Hùng', '30', N'Dũng Nam')
+insert into Lich_TourDL values ('T003', '10/03/2017', N'Nam', '45', N'Nguyễn An')
+insert into Lich_TourDL values ('T002', '28/04/2017', N'Vân', '25', N'Ngọc Dung')
+insert into Lich_TourDL values ('T004', '29/04/2017', N'Dũng', '35', N'Lê ngọc')
+insert into Lich_TourDL values ('T001', '30/04/2017', N'Nam', '25', N'Trần Nam')
+insert into Lich_TourDL values ('T003', '15/06/2017', N'Vân', '20', N'Trịnh Bá')
 select * from Lich_TourDL
